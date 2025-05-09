0cb5b1a20d3a656e5e75634fc197a926d14385fc
Thiago Sampaio <berlim0608@gmail.com>
2025-05-09 11:42:40 -0300

    Aula 09-05

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,11 @@
 const express = require('express'); 
 const router = express.Router(); 
 
-const UsuariosController = require('../controllers/usuarios'); 
+const ObjetosController = require('../controllers/objetos'); 
 
-router.get('/usuarios', UsuariosController.listarUsuarios); 
-router.post('/usuarios', UsuariosController.cadastrarUsuarios); 
-router.patch('/usuarios', UsuariosController.editarUsuarios); 
-router.delete('/usuarios', UsuariosController.apagarUsuarios); 
+router.get('/objetos', ObjetosController.listarObjetos); 
+router.post('/objetos', ObjetosController.cadastrarObjetos); 
+router.patch('/objetos', ObjetosController.editarObjetos); 
+router.delete('/objetos', ObjetosController.apagarObjetos); 
 
-
-module.exports = router
+module.exports = router;
