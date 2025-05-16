b9d01d39aef48fa367c4944d6bfc07831862c86c
Thiago Sampaio <berlim0608@gmail.com>
2025-05-16 11:37:05 -0300

    Aula 16-05

--- src/controllers/objetos.js
+++ src/controllers/objetos.js
@@ -4,12 +4,16 @@ module.exports = {
     async listarObjetos(request, response) {
         try {
 
+
             const sql = `
-              SELECT obj_id, categ_id, usu_id, obj_descricao, obj_foto, obj_local_encontrado, obj_data_publicacao, obj_status, obj_encontrado = 1 AS usu_ativo
+              SELECT obj_id, categ_id, usu_id, obj_descricao, obj_foto, obj_local_encontrado, obj_data_publicacao, obj_status, obj_encontrado = 1 AS obj_encontrado
               FROM objetos;  
             `;
 
+
             const [rows] = await db.query(sql);
+
+           
         
             return response.status(200).json({
                 sucesso: true, 
@@ -27,10 +31,36 @@ module.exports = {
     }, 
     async cadastrarObjetos(request, response) {
         try {
+
+            const {categ_id, usu_id, obj_descricao, obj_foto, obj_local_encontrado, obj_data_publicacao, obj_status} = request.body;
+            const obj_encontrado = 0;
+             
+            const sql = `
+                INSERT INTO objetos
+                    (categ_id, usu_id, obj_descricao, obj_foto, obj_local_encontrado, obj_data_publicacao, obj_status, obj_encontrado)
+                VALUES
+                    (?,?,?,?,?,?,?,?)
+            `;
+
+            const values = [categ_id, usu_id, obj_descricao, obj_foto, obj_local_encontrado, obj_data_publicacao, obj_status, obj_encontrado]
+            const [result] = await db.query(sql, values)
+            const dados = {
+                    obj_id: result.insertId,
+                    categ_id,
+                    usu_id,
+                    obj_descricao, 
+                    obj_foto,
+                    obj_local_encontrado,
+                    obj_data_publicacao,
+                    obj_status,
+                    obj_encontrado
+
+            };
+
             return response.status(200).json({
                 sucesso: true, 
                 mensagem: 'Cadastro de objetos', 
-                dados: null
+                dados: dados
             });
         } catch (error) {
             return response.status(500).json({
